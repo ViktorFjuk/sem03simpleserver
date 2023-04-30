d2f9b46b89262f1c3e9f199ad07ca0faffc93c91
ViktorFjuk <vikfju@gmail.com>
2023-04-30 19:33:21 +0000

    siste endringer

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/ViktorFjuk/sem03simpleserver
 go 1.18
 
 require github.com/ViktorFjuk/is105sem03 v0.0.0-20230423214056-27403ba27665
+
+require (
+	github.com/ViktorFjuk/funtemps v0.0.0-20230329192854-751eb45433ad // indirect
+	github.com/ViktorFjuk/minyr v0.0.0-20230416165649-1261e6b4f76e // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -6,58 +6,126 @@ import (
 	"net"
 	"sync"
 	"github.com/ViktorFjuk/is105sem03/mycrypt"
+	"strings"
+	"github.com/ViktorFjuk/minyr/yr"
 )
-
 
 func main() {
 
 
-	var wg sync.WaitGroup
+  var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.2:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
+
+  server, err := net.Listen("tcp", "172.17.0.2:8080")
+
+  if err != nil {
+
+    log.Fatal(err)
+
+  }
+
+  log.Printf("bundet til %s", server.Addr().String())
+
+  wg.Add(1)
+
+  go func() {
+
+    defer wg.Done()
+
+    for {
+
+      log.Println("før server.Accept() kallet")
+
+      conn, err := server.Accept()
+
+      if err != nil {
+
+        return
+
+      }
+
+      go func(c net.Conn) {
+
+        defer c.Close()
+
+        for {
+
+          buf := make([]byte, 1024)
+
+          n, err := c.Read(buf)
+
+          if err != nil {
+
+            if err != io.EOF {
+
+              log.Println(err)
+
+            }
+
+            return // fra for løkke
+
+          }
+
+          dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+
+ 
+
+          msg := string(dekryptertMelding)
+
+	log.Println("msg:", msg) 
+
+          switch msg {
+
+                  case "ping":
+
+        	    krypterPong := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+
+            		_, err = c.Write([]byte(string(krypterPong)))
+
+
+
+          default:
+
+			if strings.HasPrefix(msg, "Kjevik") {
+
+
+				output := yr.ProsesserLinjer(msg)
+
+
+
+            			log.Println(output)
+
+            			kryptertOutput := mycrypt.Krypter([]rune(output), mycrypt.ALF_SEM03, 4)
+
+            			_, err = c.Write([]byte(string(kryptertOutput)))
+			} else{
+
+
+	            		_, err = c.Write(buf[:n])
 			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
 
-					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-        				log.Println("Dekrypter melding: ", string(dekryptertMelding))
-        				switch msg := string(dekryptertMelding); msg {
+          }
 
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+          if err != nil {
+
+            if err != io.EOF {
+
+              log.Println(err)
+
+            }
+
+            return // fra for løkke
+
+          }
+
+        }
+
+      }(conn)
+
+    }
+
+  }()
+
+  wg.Wait()
+
 }
